d2ada7025512b111ec99b9f28d129cf2df27e78e
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2023-11-16 02:28:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Client(
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
     in_memory=True,
-    plugins=dict(root="DAXXSTRINGBOT"),
+    plugins=dict(root="ANGELSTRINGBOT"),
 )
 
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
-    print(f"@{uname} 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐒𝐔𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘. 𝐌𝐀𝐃𝐄 𝐁𝐘 @ 𝐃𝐀𝐗𝐗 𝐓𝐄𝐀𝐌 🤗")
+    print(f"@{uname} 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐒𝐔𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘. 𝐌𝐀𝐃𝐄 𝐁𝐘 @IND_PAWAN 🤗")
     idle()
     app.stop()
     print("𝗕𝗢𝗧 𝗦𝗧𝗢𝗣𝗣𝗘𝗗!")
