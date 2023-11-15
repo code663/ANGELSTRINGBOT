c1280e8e807f78677d5fae5c97ee3908f6e319bc
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2023-11-16 01:58:05 +0530

    Update start.py

--- DAXXSTRINGBOT/start.py
+++ DAXXSTRINGBOT/start.py
@@ -26,8 +26,8 @@ async def start(bot: Client, msg: Message):
                     InlineKeyboardButton(text="🌿𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆🌿", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton(" 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="https://t.me/AllPremiumCc"),
-                    InlineKeyboardButton("𝐎𝐟𝐟𝐢𝐜𝐞", url="https://t.me/CYBERDAXXX")
+                    InlineKeyboardButton(" 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="https://t.me/ANGEL_K_WORLD"),
+                    InlineKeyboardButton("𝐎𝐟𝐟𝐢𝐜𝐞", url="https://t.me/+ITHwbtkBi-Y3Yzll")
                 ]
             ]
         ),
